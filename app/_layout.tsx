faa4af0fb1e81b3b928bb6c7bee0e22d094b8e63
lookymlive <lookymlive@gmail.com>
2025-02-17 17:43:06 -0300

    Initialize Expo project with TypeScript configuration, basic navigation, and essential screens

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen 
+      name="index" 
+      options={{ 
+        headerTitle: "Sticker Smash"}} />
+      <Stack.Screen 
+      name="about" 
+      options={{ 
+        headerTitle: "About"}} />
+    </Stack>
+  );
+}
