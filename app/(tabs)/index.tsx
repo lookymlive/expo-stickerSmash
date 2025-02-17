ec149264618b0ba078f99b369d90aba61b4a888a
lookymlive <lookymlive@gmail.com>
2025-02-17 19:43:18 -0300

    Add Button component and integrate it into the tab layout; update styles and structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { View, StyleSheet } from 'react-native';
 
-import ImageViewer from "@/components/ImageViewer";
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 
-import PlaceholderImage from "../../assets/images/background-image.png";
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -17,10 +22,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
