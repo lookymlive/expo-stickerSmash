bc24f55afbc2790158dd16c23f384b7f6152ff6f
lookymlive <lookymlive@gmail.com>
2025-02-17 18:07:20 -0300

    Refactor navigation structure and remove deprecated screens; add tab layout for improved user experience

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,8 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen 
-      name="index" 
-      options={{ 
-        headerTitle: "Sticker Smash"}} />
-      <Stack.Screen 
-      name="about" 
-      options={{ 
-        headerTitle: "About"}} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
